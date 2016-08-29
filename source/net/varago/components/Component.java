631e88c459773cf7c3622b272e9b46839cb68fdb
Zaen Khilji <zaen.khilji@gmail.com>
2016-08-29 10:00:08 +0200

    Component handling

---
+++ source/net/varago/components/Component.java
@@ -0,0 +1,11 @@
+package net.varago.components;
+
+public abstract class Component {
+	
+	/**
+	 * @author: Zaen Khilji
+	 */
+	
+	public abstract void add();
+
+}
---
+++ source/net/varago/components/impl/CheckBox.java
@@ -0,0 +1,15 @@
+package net.varago.components.impl;
+
+import net.varago.components.Component;
+
+public class CheckBox extends Component {
+	
+	/**
+	 * @author: Zaen Khilji
+	 */
+
+	@Override
+	public void add() {
+		
+	}
+}
